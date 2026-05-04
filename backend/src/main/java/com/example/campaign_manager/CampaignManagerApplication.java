967e0ab82616268567a04aac584e16679b5dcd46
pxyvlrd <priv.hubertjucha@gmail.com>
2026-05-04 20:45:48 +0200

    chore: initialize Spring Boot project structure

---
+++ backend/src/main/java/com/example/campaign_manager/CampaignManagerApplication.java
@@ -0,0 +1,13 @@
+package com.example.campaign_manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CampaignManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CampaignManagerApplication.class, args);
+	}
+
+}
